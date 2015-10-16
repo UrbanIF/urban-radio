19a2578b02f755622d8b3e644ebc4ee22a410fd9
Rostyslav Diachok <infernalmaster1@gmail.com>
2015-10-17 01:49:05 +0300

    init

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'urban-radio',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://urban-radio.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
