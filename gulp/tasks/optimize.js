330557118cc3031b06cf281f3fa4a84d186bc839
Rostyslav Diachok <infernalmaster1@gmail.com>
2015-11-04 02:56:15 +0200

    fixes

--- gulp/tasks/optimize.js
+++ gulp/tasks/optimize.js
@@ -48,9 +48,9 @@ gulp.task('optimize', function() {
     .pipe(useref())
 
 
-    // // Update some path. Use that if you want open html pages from dist directory localy without server
-    // // or comment this 2 lines for using on server
-    // .pipe(gulpIf('*.html', gulpReplace('/assets/', 'assets/')))
+    // Update some path. Use that if you want open html pages from dist directory localy without server
+    // or comment this 2 lines for using on server
+    .pipe(gulpIf('*.html', gulpReplace('/assets/', 'assets/')))
     // .pipe(gulpIf('*.css', gulpReplace('/assets/', 'assets/')))
     // .pipe(gulpIf('*.js', gulpReplace('/assets/', 'assets/')))
 
